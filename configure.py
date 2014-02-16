15cd4edf2baf294a78c6311836a0b232e3e4f182
Somsubhra Bairi <somsubhra.bairi@gmail.com>
2014-02-16 21:24:48 +0530

    Import database schema in configure itself Addresses Issue #34 and may ease up priority for Issue #31

--- configure.py
+++ configure.py
@@ -3,11 +3,27 @@
 import os
 import getpass
 
-def main():
-    # Take all the configuration inputs from user
+def setup():
+    # Take all the configuration inputs for database
     dbHost = raw_input("Enter database host(usually localhost): ")
     dbUsername = raw_input("Enter database username(usually root): ")
     dbPassword = getpass.getpass("Enter database password: ")
+
+    dbSetup = raw_input("Import database schema now? (y/n): ")
+
+    # If import database flag set
+
+    if(dbSetup == 'y'):
+        print("Checking mysql connection...")
+        # Import schema
+        if(os.system("mysql -h "+dbHost+" -u "+dbUsername+" -p"+dbPassword+" < db/schema.sql")):
+            print("Error: Wrong username/password/host. Please check again...Aborted")
+            exit()
+        else:
+            print("User authenticated. Connected to mysql server...")
+            print("Database at db/schema.sql imported successfully...")
+
+    # Enter admin credentials
     adminUsername = raw_input("Enter new admin username: ")
     adminPassword = getpass.getpass("Enter new admin password: ")
 
@@ -28,8 +44,10 @@ def main():
     f.close()
 
     print("Configuring project...DONE")
-    print("Configuration has been written to var/congif.php. Edit that file for any mistakes or run configure.py again.")
+    print("Configuration has been written to var/config.php. Edit that file for any mistakes or run configure.py again.")
 
+def main():
+    setup()
 
 if __name__ == "__main__":
     main()

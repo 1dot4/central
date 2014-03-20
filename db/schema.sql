af6d7a3e6dfb91cb13f633fb9dee04a5632be9e0
Somsubhra Bairi <somsubhra.bairi@gmail.com>
2014-03-20 23:55:45 +0530

    Add db schema for user types. TODO: Skill and id proof from seeker left

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,16 @@
 CREATE DATABASE IF NOT EXISTS central;
 USE central;
 
+CREATE TABLE IF NOT EXISTS `organization` (
+  `name` varchar(100) NOT NULL UNIQUE,
+  PRIMARY KEY (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 ;
+
+CREATE TABLE IF NOT EXISTS `location` (
+  `name` varchar(100) NOT NULL UNIQUE,
+  PRIMARY KEY (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 ;
+
 CREATE TABLE IF NOT EXISTS `user` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `username` varchar(300) NOT NULL,
@@ -12,20 +22,40 @@ CREATE TABLE IF NOT EXISTS `user` (
 
 CREATE TABLE IF NOT EXISTS `volunteer` (
   `id` bigint(20) NOT NULL,
+  `fullname` text,
+  `email` text,
+  `org_name` varchar(100),
+  `designation` text,
+  `location_name` varchar(100),
   PRIMARY KEY (`id`),
-  FOREIGN KEY (`id`) REFERENCES user(`id`)
+  FOREIGN KEY (`id`) REFERENCES user(`id`),
+  FOREIGN KEY (`org_name`) REFERENCES organization(`name`),
+  FOREIGN KEY (`location_name`) REFERENCES location(`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 ;
 
 CREATE TABLE IF NOT EXISTS `provider` (
   `id` bigint(20) NOT NULL,
+  `fullname` text,
+  `email` text,
+  `org_name` varchar(100),
+  `designation` text,
+  `location_name` varchar(100),
   PRIMARY KEY (`id`),
-  FOREIGN KEY (`id`) REFERENCES user(`id`)
+  FOREIGN KEY (`id`) REFERENCES user(`id`),
+  FOREIGN KEY (`org_name`) REFERENCES organization(`name`),
+  FOREIGN KEY (`location_name`) REFERENCES location(`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 ;
 
 CREATE TABLE IF NOT EXISTS `seeker` (
   `id` bigint(20) NOT NULL,
+  `fullname` text,
+  `experience` int(4),
+  `pref_location_name` varchar(100),
+  `curr_location_name` varchar(100),
   PRIMARY KEY (`id`),
-  FOREIGN KEY (`id`) REFERENCES user(`id`)
+  FOREIGN KEY (`id`) REFERENCES user(`id`),
+  FOREIGN KEY (`pref_location_name`) REFERENCES location(`name`),
+  FOREIGN KEY (`curr_location_name`) REFERENCES location(`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 ;
 
 CREATE TABLE IF NOT EXISTS `verificationStatus` (

47b32b4f57057027a810cccbad25e39e92ae23b6
Somsubhra Bairi <somsubhra.bairi@gmail.com>
2014-03-24 14:51:57 +0530

    Add template variables in MyProfilePageController

--- controllers/MyProfilePageController.php
+++ controllers/MyProfilePageController.php
@@ -31,13 +31,48 @@ class MyProfilePageController extends Controller {
         $type = $user->type();
 
         $this->setVar("userType", $type);
+        $this->setVar('username', $user->username());
+        $this->setVar('phone', $user->phone());
+        $this->setVar('fullName', $user->fullName());
 
         switch($type) {
+
             case 'seeker':
+
+                require_once 'models/Seeker.php';
+                $user = new Seeker($id);
+
+                // Set seeker specific variables
+                $this->setVar('prefLocation', $user->preferredLocation());
+                $this->setVar('currLocation', $user->currentLocation());
+                $this->setVar('experience', $user->experience());
+
                 break;
+
             case 'provider':
+
+                require_once 'models/Provider.php';
+                $user = new Provider($id);
+
+                // Set Provider specific variables
+                $this->setVar('email', $user->email());
+                $this->setVar('organization', $user->organization());
+                $this->setVar('designation', $user->designation());
+                $this->setVar('location', $user->location());
+
                 break;
+
             case 'volunteer':
+
+                require_once 'models/Volunteer.php';
+                $user = new Volunteer($id);
+
+                // Set the Volunteer specific variables
+                $this->setVar('email',$user->email());
+                $this->setVar('organization', $user->organization());
+                $this->setVar('designation', $user->designation());
+                $this->setVar('location', $user->location());
+
                 break;
         }
     }

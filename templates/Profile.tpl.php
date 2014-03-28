0160b9d7845da14a86038ac99cc2c59709a6fe08
Somsubhra Bairi <somsubhra.bairi@gmail.com>
2014-03-28 17:58:27 +0530

    Solve the Level problem in header. Part of profile page

--- templates/Profile.tpl.php
+++ templates/Profile.tpl.php
@@ -5,16 +5,41 @@
             <?php echo $title ?>
         </title>
         <?php require_once 'include/CssLevel2.php' ?>
+        <style>
+            .col-md-10 {
+                margin-top: 100px;
+            }
+            hr {
+                border-top: 1px solid #dddddd;
+            }
+        </style>
     </head>
     <body>
         <div class="container">
             <?php
             require_once 'include/PrintUtils.php';
-            printNavBar('home', $currentUserName)
+            printNavBar('profile', $currentUserName, 2)
             ?>
             <div class="row">
                 <div class="col-md-10 well">
+                    <div class="row">
+                        <div class="col-md-3">
 
+                        </div>
+                        <div class="col-md-7">
+                            <h1><?php echo $username ?></h1>
+                            <hr>
+                            <?php if($fullName != ""): ?>
+                            <h4><b><?php echo $fullName ?></b></h4>
+                            <?php endif ?>
+
+                            <?php if($userType == 'volunteer'): ?>
+                                <?php if($organization != ""): ?>
+                                    <h4>Works at <b><?php echo $organization ?></b></h4>
+                                <?php endif?>
+                            <?php endif ?>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
--- templates/include/PrintUtils.php
+++ templates/include/PrintUtils.php
@@ -1,6 +1,13 @@
 <?php
 
-function printNavBar($page, $username) {
+function printNavBar($page, $username, $level = 1) {
+
+    if($level == 1) {
+        $dir = ".";
+    } elseif($level == 2) {
+        $dir = "..";
+    }
+
     echo '<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
             <div class="container-fluid">
                 <div class="navbar-header">
@@ -10,7 +17,7 @@ function printNavBar($page, $username) {
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
                     </button>
-                    <a class="navbar-brand" href="./">janrozgaar.in</a>
+                    <a class="navbar-brand" href="'.$dir.'/">janrozgaar.in</a>
                 </div>
                 <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                     <form class="navbar-form navbar-left" role="search">
@@ -19,14 +26,14 @@ function printNavBar($page, $username) {
                         </div>
                         </form>
                     <ul class="nav navbar-nav navbar-right">
-                        <li><a href="home">Home</a></li>
-                        <li><a href="myprofile">'.$username.'</a></li>
+                        <li><a href="'.$dir.'/home">Home</a></li>
+                        <li><a href="'.$dir.'/myprofile">'.$username.'</a></li>
                         <li class="dropdown">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown"><b class="caret"></b></a>
                             <ul class="dropdown-menu">
                                 <li><a href="#">Help</a></li>
                                 <li class="divider"></li>
-                                <li><a href="logout">Logout</a></li>
+                                <li><a href="'.$dir.'/logout">Logout</a></li>
                             </ul>
                         </li>
                     </ul>

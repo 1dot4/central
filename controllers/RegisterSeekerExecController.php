2dea344cdb6871d318ebfe612bf6d23dc85db327
Jagrut <jktrivedi29@gmail.com>
2014-04-03 16:25:42 -0500

    volunteer validation complete

--- controllers/RegisterSeekerExecController.php
+++ controllers/RegisterSeekerExecController.php
@@ -6,10 +6,10 @@
  */
 class RegisterSeekerExecController extends ExecController {
 
-    private function validateRegistration($username, $phone, $password, $cpassword) {
+    private function validateRegistration($username, $phone, $password, $cpassword,$currentLocation,$preferredLocation,$skills,$experience) {
         $error = false;
         $errMsg = "";
-
+		$t=0;
         // Check for username uniqueness
         require_once 'models/User.php';
 
@@ -31,9 +31,25 @@ class RegisterSeekerExecController extends ExecController {
 
         if(strlen($phone) != 10 || !is_numeric($phone)) {
             $error = true;
-            $errMsg .= "Invalid Phone Number<br>";
+            $errMsg .= "Invalid PhoneNumber<br>";
         }
-
+		
+		if (!preg_match("/^[a-zA-Z ]*$/",$currentLocation))
+		{
+		$error=true;
+		$errMsg .= "In location Only letters are allowed.<br>"; 
+		$t=1;
+		}
+		
+		if (!preg_match("/^[a-zA-Z ]*$/",$preferredLocation))
+		{
+		$error=true;
+		if($t==0)
+			$errMsg .= "In location Only letters are allowed.<br>"; 
+		}
+		
+		
+		
 		if(strlen($password)< 6) {
 			$error = true;
             $errMsg .= "Password of minimum 6 characters is required.<br>";
@@ -44,11 +60,25 @@ class RegisterSeekerExecController extends ExecController {
             $errMsg .= "Passwords do not match.<br>";
         }
 
+		if(!preg_match("/^[0-9]*$/",$experience))
+		{
+			$error = true;
+            $errMsg .= "Experience must be integer.<br>";
+    	
+		}
+		
+		if (!preg_match("/^[a-zA-Z ]*$/",$skills))
+		{
+		$error=true;
+		$errMsg .= "In skills Only letters are allowed.<br>"; 
+		}
+		
         if($error) {
             require_once 'libs/Session.php';
             Session::start();
             Session::setVar("ERR_MSG", $errMsg);
             Session::close();
+			$this->setRedirectUri('./home/register');
         }
 
         return !$error;
@@ -72,7 +102,7 @@ class RegisterSeekerExecController extends ExecController {
         $skillSet = explode(',', $skills);
 
         // Validation stuff
-        if(!($this->validateRegistration($username, $phone, $password, $cpassword))) {
+        if(!($this->validateRegistration($username, $phone, $password, $cpassword,$currentLocation,$preferredLocation,$skills,$experience))) {
             $this->setRedirectUri("home");
         }
 

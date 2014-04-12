544b66b51be70f8fe081a4084bf86a8f0d380307
Somsubhra Bairi <somsubhra.bairi@gmail.com>
2014-04-12 14:40:41 +0530

    Job edit page controller process

--- controllers/JobEditPageController.php
+++ controllers/JobEditPageController.php
@@ -12,6 +12,37 @@ class JobEditPageController extends PageController {
      */
     protected function process() {
 
+        require_once 'libs/Auth.php';
+        $userId = Auth::userId();
+
+        $jobId = $this->param();
+
+        require_once 'models/Job.php';
+        $job = new Job($jobId);
+
+        if($job->postedById() != $userId) {
+            $this->app()->response->redirect("/permission.denied");
+        }
+
+        $skillString = "";
+
+        foreach($job->skills() as $skill) {
+            $skillString .= $skill . ",";
+        }
+
+        $this->setVar('title', $job->title());
+        $this->setVar('description', $job->description());
+        $this->setVar('skills', $skillString);
+        $this->setVar('positions', $job->positions());
+        $this->setVar('location', $job->location());
+        $this->setVar('type', $job->type());
+        $this->setVar('startDate', $job->startTime());
+
+        if($job->type() == 'temporary') {
+            require_once 'models/TemporaryJob.php';
+            $job = new TemporaryJob($jobId);
+            $this->setVar('duration', $job->duration());
+        }
     }
 
 }

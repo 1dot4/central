df3dcc84b640cc79a5e923412052fcb1d18d4dba
Nitin Garg <nick.garg28@gmail.com>
2014-04-14 05:07:25 +0530

    changed favicon

--- templates/include/CssLevel2.php
+++ templates/include/CssLevel2.php
@@ -1,4 +1,4 @@
 <?php
-echo '<link rel="icon" href="../public/images/favicon.ico" type="image/icon" />';
+echo '<link rel="icon" href="../public/images/faviconFinal.ico" type="image/icon" />';
 echo '<link rel="stylesheet" href="../public/bootstrap/css/bootstrap.min.css" />';
 echo '<link rel="stylesheet" href="../public/bootstrap/css/bootstrap-theme.min.css" />';

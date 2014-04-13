4698adf91c4d62ac4f5f0825552dc5c5e1aa669c
Somsubhra Bairi <somsubhra.bairi@gmail.com>
2014-04-13 11:31:21 +0530

    Fav notif

--- controllers/FavoriteStreamController.php
+++ controllers/FavoriteStreamController.php
@@ -22,6 +22,11 @@ class FavoriteStreamController extends StreamController {
 
         if(!$user->hasFavorited($favoritedId)) {
             $user->favorite($favoritedId);
+
+            require_once 'models/Notification.php';
+            $message = "<a href='../profile/" . $user->username() . "'>" . $user->username() . "</a> has added you to favourites.";
+            Notification::newNotification($favoritedId, $message);
+
             $record = Array(
                 'success' => true,
                 'message' => 'Favourited'

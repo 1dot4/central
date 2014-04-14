d4ff5032f917c444498ef82ec940a6cfdddfee0c
Somsubhra Bairi <somsubhra.bairi@gmail.com>
2014-04-14 15:58:32 +0530

    Fix css

--- templates/Job.tpl.php
+++ templates/Job.tpl.php
@@ -8,13 +8,13 @@
         <style>
             .col-md-8 {
                 margin-top: 100px;
-                margin-left: 200px;
+                margin-left: 180px;
             }
             hr {
                 border-top: 1px solid #dddddd;
             }
             #edit-btn {
-                margin-left: 350px;
+                margin-left: 320px;
             }
             .dp-box {
             }
@@ -41,19 +41,15 @@
                 require_once 'include/PrintUtils.php';
                 printNavBar('profile', $username, 2)
             ?>
-            <div class="row">
-                <div class="col-md-8">
-                    <div class="row">
-                        <?php
-                            printJob($jobArray, $userId);
+            <div class="col-md-8">
+                <?php
+                printJob($jobArray, $userId);
 
-                            if($userId == $jobPosterId) {
-                                echo "<hr>";
-                                printUsers($seekers);
-                            }
-                        ?>
-                    </div>
-                </div>
+                if($userId == $jobPosterId) {
+                    echo "<hr>";
+                    printUsers($seekers);
+                }
+                ?>
             </div>
         </div>
         <?php require_once 'include/ScriptsLevel2.php' ?>

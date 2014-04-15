54a8a0eba6fb117dedd05a5d93776b6237ea46a3
Somsubhra Bairi <somsubhra.bairi@gmail.com>
2014-04-15 12:39:04 +0530

    Added gauge

--- templates/SeekerHome.tpl.php
+++ templates/SeekerHome.tpl.php
@@ -123,6 +123,9 @@
                                 }
                             ?>
                         </div>
+                        <div class="col-md-4">
+                            <canvas id="profile-meter"></canvas>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -130,7 +133,29 @@
         <?php require_once 'include/ScriptsLevel2.php' ?>
         <script src="../public/tagsinput/js/bootstrap-tagsinput.js"></script>
         <script src="../public/tagsinput/js/typeahead.min.js"></script>
+        <script src="../public/guage/gauge.min.js"></script>
         <script>
+            var opts = {
+                lines: 12, // The number of lines to draw
+                angle: 0.15, // The length of each line
+                lineWidth: 0.44, // The line thickness
+                pointer: {
+                    length: 0.9, // The radius of the inner circle
+                    strokeWidth: 0.035, // The rotation offset
+                    color: '#000000' // Fill color
+                },
+                limitMax: 'false',   // If true, the pointer will not go past the end of the gauge
+                colorStart: '#6FADCF',   // Colors
+                colorStop: '#8FC0DA',    // just experiment with them
+                strokeColor: '#E0E0E0',   // to see which ones work best for you
+                generateGradient: true
+            };
+            var target = document.getElementById('profile-meter'); // your canvas element
+            var gauge = new Gauge(target).setOptions(opts); // create sexy gauge!
+            gauge.maxValue = 3000; // set max gauge value
+            gauge.animationSpeed = 39; // set animation speed (32 is default value)
+            gauge.set(100); // set actual value
+
             function expressInterest(jobId) {
                 $.get("../job.interested/" + jobId + "?value=true").done(function(data) {
                     //console.log(data);
